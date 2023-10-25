f89891d153ce250f6c9e723cc2bae2fc615a0816
elysthetruechi <kaluelizabeth29@gmail.com>
2023-10-26 00:14:24 +0100

    ,

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Item extends React.Component{
+  render(){
+    return(
+      <h1>Heck, yeah i breathe you!</h1>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Item />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GOD IS GOOD
         </a>
       </header>
     </div>
